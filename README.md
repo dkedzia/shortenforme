ee1c46a5c2de95e6b1531bd0566568ee6bb478f5
Dominik Kędzia <dominikkedzia@gmail.com>
2023-02-09 16:02:56 +0100

    README update

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# shortenforme
+# Shorten for me
+
+Available live - http://shortenfor.me
+
+URL shorting service
+
++ Monolith
++ Maximum URL length - `1000` characters
++ Feature and unit tests are available under `/test` directory
++ No JavaScript required
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,6 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Shorten for me</title>
-        <!-- Bootstrap CSS -->
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
         <link rel="stylesheet" type="text/css" href="{{ asset('css/style.css') }}">
     </head>
