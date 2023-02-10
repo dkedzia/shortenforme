db678dbe7e358cbde0f09aefe4459501907be708
Dominik Kędzia <dominikkedzia@gmail.com>
2023-02-10 19:56:00 +0100

    Add local bootstrap

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Shorten for me</title>
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+        <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
         <link rel="stylesheet" type="text/css" href="{{ asset('css/style.css') }}">
     </head>
     <body>
@@ -13,7 +13,6 @@
                 @csrf
                 <div class="form-group">
                     <input type="text" class="form-control {{ $errors->has('origin_url') ? 'error' : '' }}" name="origin_url" id="origin_url" placeholder="URL to be shortened...">
-                    <!-- Error -->
                     @if ($errors->has('origin_url'))
                         <div class="error">
                             {{ $errors->first('origin_url') }}
