8e9fb931bd9647227480fe5ba5b57d558f9cd30b
Dominik Kędzia <dominikkedzia@gmail.com>
2023-02-11 21:44:11 +0100

    Frontend copy to clipboard

--- resources/views/aliasAdded.blade.php
+++ resources/views/aliasAdded.blade.php
@@ -6,11 +6,28 @@
         <title>Shorten for me</title>
         <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
         <link rel="stylesheet" type="text/css" href="{{ asset('css/style.css') }}">
+        <script>
+            function copyUrlToClipboard() {
+                window.navigator.clipboard.writeText('{{ $shortenedUrl }}');
+                const copyToClipboard = document.querySelector('#copy_to_clipboard')
+                copyToClipboard.textContent = 'Copied!'
+                setTimeout(() => {
+                    copyToClipboard.textContent = 'Copy'
+                }, 3000)
+            }
+        </script>
     </head>
     <body>
         <div class="container mt-5">
             Your shortened url:
             <h2>{{ $shortenedUrl }}</h2>
+            <div
+                id="copy_to_clipboard"
+                class="btn btn-dark btn-block"
+                onclick="copyUrlToClipboard()"
+            >
+                Copy
+            </div>
         </div>
     </body>
 </html>
