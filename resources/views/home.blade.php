6cc900da22f9d4dde1a222819416d732c525d221
Dominik Kędzia <dominikkedzia@gmail.com>
2023-02-11 20:52:42 +0100

    Frontend parsing

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,13 +6,42 @@
         <title>Shorten for me</title>
         <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
         <link rel="stylesheet" type="text/css" href="{{ asset('css/style.css') }}">
+        <script>
+            document.addEventListener("DOMContentLoaded", () => {
+                document.querySelector('#origin_url').setAttribute('type', 'text')
+                document.querySelector('#protocol_tip').setAttribute('style', 'display: none;')
+            });
+
+            function formSubmitValidator() {
+                const originUrl = document.shortenForMeForm.origin_url.value;
+                if (!originUrl.startsWith('http') && !originUrl.startsWith('https')) {
+                    document.shortenForMeForm.origin_url.value = 'https://' + originUrl
+                }
+                return true
+            }
+        </script>
     </head>
     <body>
         <div class="container mt-5">
-            <form method="post" action="{{ route('aliases.addNew') }}">
+            <form
+                method="post"
+                action="{{ route('aliases.addNew') }}"
+                name="shortenForMeForm"
+                onsubmit="formSubmitValidator(this)"
+            >
                 @csrf
                 <div class="form-group">
-                    <input type="url" class="form-control {{ $errors->has('origin_url') ? 'error' : '' }}" name="origin_url" id="origin_url" placeholder="URL to be shortened...">
+                    <input
+                        type="url"
+                        class="form-control {{ $errors->has('origin_url') ? 'error' : '' }}"
+                        name="origin_url"
+                        id="origin_url"
+                        placeholder="URL to be shortened..."
+                    >
+                    <p
+                        id="protocol_tip"
+                        style="font-size: 12px; font-weight: bold; margin: 5px 0;"
+                    >Tip: Don't forget the scheme (e.g. https://)</p>
                     @if ($errors->has('origin_url'))
                         <div class="error">
                             {{ $errors->first('origin_url') }}
